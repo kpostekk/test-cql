78344b84b8106abad21cdce68db954941c2d4a48
Aleksandra Indulska <aleksandra.indulska@gmail.com>
2022-08-21 14:09:05 +0200

    aaaaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from sqlite3 import connect
 
 app = FastAPI()
+db = connect('database.db')
 
 @app.post('/exec')
 def exec_any(code: str):
     return {'result': eval(code)}
+
+@app.post('/create-cat')
+def create_cat(name: str):
+    cursor = db.cursor()
+    cursor.execute('INSERT INTO cats (name) VALUES (?)', (name,))
+    db.commit()
+    return {'result': 'ok'}
+
+
+@app.get('/get-cats')
+def get_cats(name: str = None):
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM cats WHERE name LIKE ?', (name,))
+    return {'result': cursor.fetchall()}
+
 
 if __name__ == '__main__':
     import uvicorn
